7d1a3ae4e4e5c54caa8e4b001dc1f67df5639f09
Aleksejs Jurenoks <40733171+ajurenoks@users.noreply.github.com>
2024-02-11 19:27:51 +0200

    Add files via upload

---
+++ LL_1.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+        
+
+    # WRITE FIND_MIDDLE_NODE METHOD HERE #
+    #                                    #
+    #                                    #
+    #                                    #
+    #                                    #
+    ######################################
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print( my_linked_list.find_middle_node().value )
+
